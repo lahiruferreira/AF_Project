01e3002b7381153c511fc2e41cf0e171bf894e6a
lahiruferreira <lahriuferreira444@gmail.com>
2020-05-13 22:53:42 +0530

    page navigate 1

---
+++ client/src/Pages/Feedback.js
@@ -0,0 +1,11 @@
+import React from "react";
+//import {Link} from "react-router-dom";
+
+const Feedback = () => {
+    return (
+        <h1>FeedBack Page 1111</h1>
+    );
+}
+
+
+export default (Feedback);
